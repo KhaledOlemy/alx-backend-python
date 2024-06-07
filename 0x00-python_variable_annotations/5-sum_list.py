ac5195d147ca924b11b52d3c548bc365be112dd1
khaledolemy <khaledolemy@gmail.com>
2024-06-07 03:31:35 +0300

    typing!

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,7 +2,8 @@
 """
 sum_list float to float
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     return sum(input_list)
