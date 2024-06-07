99b0e924b6685ec40438d792a85a5d574967326a
khaledolemy <khaledolemy@gmail.com>
2024-06-07 03:39:58 +0300

    Union + Tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+xx
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    kv to kv^2
+    """
+    return (k, v**2)
