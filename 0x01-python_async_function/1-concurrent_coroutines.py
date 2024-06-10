1caef08cb344d4c9de9b2aa0aca05ce80fcf794e
khaledolemy <khaledolemy@gmail.com>
2024-06-10 07:53:04 +0300

    add task2 + type annotations + pycodestyle

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Task: 1, make concurrent co-routines
+"""
+import asyncio
+import typing
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """
+    Task: 1, make concurrent co-routines
+    args: n: int, max_delay: int.
+    return: typing.List of floats.
+    """
+    output: typing.List = []
+    for _ in range(n):
+        output.append(await wait_random(max_delay))
+    return output
