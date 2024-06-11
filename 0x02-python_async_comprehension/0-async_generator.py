6fc3fea59773ce0d85c8bd3a05023637d53fc94d
khaledolemy <khaledolemy@gmail.com>
2024-06-11 12:06:54 +0300

    adding comment regarding the correct way

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,6 +12,8 @@ async def async_generator() -> typing.Generator[float, None, None]:
     Task: 0, The coroutine will loop 10 times,
     each time asynchronously wait 1 second, then
     yield a random number between 0 and 10
+    return type should be:
+    typing.AsyncGenerator[float, None]
     """
     for _ in range(10):
         await asyncio.sleep(1)
